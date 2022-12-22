3a0a4a0e988902e04ab914a94ad3dc725d94fae7
Gian Paucar <gpaucarcortez@gmail.com>
2022-12-21 21:42:28 -0500

    Primera parte

---
+++ src/funciones/Main.java
@@ -0,0 +1,17 @@
+package funciones;
+
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(1, 2, 3);
+
+        System.out.println(resultado);
+
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+
+}
